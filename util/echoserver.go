aaf1ca4c54874ed3f2b432c223b225f9ffcd9285
nishiwode322 <2281826581@qq.com>
2020-04-06 09:54:38 +0800

    ...

--- util/echoserver.go
+++ util/echoserver.go
@@ -8,13 +8,13 @@ import (
 
 func echoHandler(writer http.ResponseWriter, read *http.Request) {
 	clientData, _ := ioutil.ReadAll(read.Body)
-	fmt.Println("server receive data:", clientData)
+	fmt.Println("server receive data: ", clientData)
 	writer.Write(clientData)
 }
 
 func EchoServer(ipAndPort string) {
 	http.HandleFunc("/", echoHandler)
-	fmt.Println("echo server is listening:" + ipAndPort)
+	fmt.Println("echo server is listening: " + ipAndPort)
 	if err := http.ListenAndServe(ipAndPort, nil); err != nil {
 		fmt.Println("ListenAndServe err! ", err)
 	}
